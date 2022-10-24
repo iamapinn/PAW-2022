6bc269a8a0102e1532b8c8293dc62678a2b58d84
iamapinn <Muh.Muqtafin.Nuha.com>
2022-10-24 09:00:32 +0700

    update pertemuan 9

---
+++ Pertemuan-9_Node/myfirst.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World! My name apinn');
+}).listen(8080);
--- Pertemuan-9_Node/penjualan.php
+++ Pertemuan-9_Node/penjualan.php
@@ -1,16 +1,13 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "campus");
+$conn = mysqli_connect("localhost", "root", "", "paw_apinn");
 
 // $sql = "SELECT id_mhs, nrp, nama_mhs, nama_prodi, alamat_mhs FROM tbl_mhs m, tbl_prodi p WHERE m.id_prodi = p.id_prodi ORDER BY id_mhs ASC";
 // menggabungkan 2 tabel
-$sql_tbl = "SELECT * FROM tbl_mhs m, tbl_prodi p WHERE m.id_prodi = p.id_prodi ORDER BY id_mhs ASC";
+$sql_tbl = "SELECT * FROM Tbl_penjualan ORDER BY id_penjualan ASC";
 $result_tbl = mysqli_query($conn, $sql_tbl);
 $result_tbl_edit = mysqli_query($conn, $sql_tbl);
 $result_tbl_delete = mysqli_query($conn, $sql_tbl);
 
-// tampilkan tbl_prodi
-$sql_prodi = "SELECT * FROM tbl_prodi";
-$result_prodi = mysqli_query($conn, $sql_prodi);
 
 $count = mysqli_num_rows($result_tbl);
 ?>
@@ -22,7 +19,7 @@ $count = mysqli_num_rows($result_tbl);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa</title>
+    <title>Data Penjualan</title>
 
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -34,7 +31,7 @@ $count = mysqli_num_rows($result_tbl);
 <body>
 
     <div class="container mt-3">
-        <h2>Data Mahasiswa</h2>
+        <h2>Data Penjualan</h2>
         <p>Data ini dari MySQL : <?= $count; ?> data</p>
         <button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#ModalTambah">
             Tambah
@@ -43,10 +40,10 @@ $count = mysqli_num_rows($result_tbl);
             <thead>
                 <tr>
                     <th>ID</th>
-                    <th>NIM</th>
-                    <th>Nama Mahasiswa</th>
-                    <th>Fakultas</th>
-                    <th>Alamat</th>
+                    <th>Nama Barang</th>
+                    <th>QTY</th>
+                    <th>Harga Satuan</th>
+                    <th>Sub Total</th>
                     <th>Action</th>
                 </tr>
             </thead>
@@ -55,16 +52,21 @@ $count = mysqli_num_rows($result_tbl);
                 while ($row = $result_tbl->fetch_assoc()) {
                 ?>
                     <tr>
-                        <td><?= $row["id_mhs"]; ?></td>
-                        <td><?= $row["nrp"] ?></td>
-                        <td><?= $row["nama_mhs"]; ?></td>
-                        <td><?= $row["nama_prodi"]; ?></td>
-                        <td><?= $row["alamat_mhs"]; ?></td>
+                        <td><?= $row["id_penjualan"]; ?></td>
+                        <td><?= $row["nama_barang"] ?></td>
+                        <td><?= $row["qty"]; ?></td>
+                        <td><?= $row["harga_satuan"]; ?></td>
                         <td>
-                            <button type="button" class="btn btn-outline-success btn-sm" data-bs-toggle="modal" data-bs-target="#ModalEdit<?= $row["id_mhs"]; ?>">
+                            <?php
+                            $sub_total = $row["qty"] * $row["harga_satuan"];
+                            echo $sub_total;
+                            ?>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-outline-success btn-sm" data-bs-toggle="modal" data-bs-target="#ModalEdit<?= $row["id_penjualan"]; ?>">
                                 Edit
                             </button>
-                            <button type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#ModalDelete<?= $row["id_mhs"]; ?>">
+                            <button type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#ModalDelete<?= $row["id_penjualan"]; ?>">
                                 Delete
                             </button>
                         </td>
@@ -89,26 +91,16 @@ $count = mysqli_num_rows($result_tbl);
                     <!-- Modal body -->
                     <div class="modal-body">
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="nim" name="nim" placeholder="Masukkan NIM" required autofocus>
-                            <label for="nim" class="form-label">NIM</label>
+                            <input type="text" class="form-control" id="nama_barang" name="nama_barang" placeholder="Masukkan Nama Barang" required autofocus>
+                            <label for="nama_barang" class="form-label">Nama Barang</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="nama_mhs" name="nama_mhs" placeholder="Masukkan Nama Mahasiswa" required>
-                            <label for="nama_mhs" class="form-label">Nama Mahasiswa</label>
+                            <input type="text" class="form-control" id="qty" name="qty" placeholder="Masukkan QTY" required>
+                            <label for="qty" class="form-label">QTY</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <select class="form-select" aria-label="Floating label select example" id="id_prodi" name="id_prodi" required>
-                                <option disabled selected>Pilih Fakultas</option>
-                                <?php
-                                while ($row_prodi = $result_prodi->fetch_assoc()) { ?>
-                                    <option value="<?= $row_prodi["id_prodi"]; ?>"> <?= $row_prodi["nama_prodi"]; ?> </option>
-                                <?php } ?>
-                            </select>
-                            <label for="id_prodi">Fakultas</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <textarea class="form-control" placeholder="Masukkan Alamat" name="alamat_mhs" id="alamat_mhs" style="height: 100px"></textarea>
-                            <label for="alamat_mhs">Alamat</label>
+                            <input type="text" class="form-control" id="harga_satuan" name="harga_satuan" placeholder="Masukkan Harga Satuan" required>
+                            <label for="harga_satuan" class="form-label">Harga Satuan</label>
                         </div>
                     </div>
 
@@ -122,26 +114,25 @@ $count = mysqli_num_rows($result_tbl);
                 <!-- tambah data ke database dengan button tambah -->
                 <?php
                 if (isset($_POST["tambah"])) {
-                    $nim = $_POST["nim"];
-                    $nama_mhs = $_POST["nama_mhs"];
-                    $id_prodi = $_POST["id_prodi"];
-                    $alamat_mhs = $_POST["alamat_mhs"];
+                    $nama_barang = $_POST["nama_barang"];
+                    $qty = $_POST["qty"];
+                    $harga_satuan = $_POST["harga_satuan"];
 
-                    $sql_Select = "SELECT * FROM tbl_mhs WHERE nrp = '$nim'";
+                    $sql_Select = "SELECT * FROM Tbl_penjualan WHERE nama_barang = '$nama_barang'";
                     $result_Select = mysqli_query($conn, $sql_Select);
 
                     if (mysqli_num_rows($result_Select) > 0) {
                         echo "<script>
-                                    alert('NIM sudah ada');
+                                    alert('Barang sudah ada');
                                 </script>";
                     } else {
-                        $sql_Insert = "INSERT INTO tbl_mhs (nrp, nama_mhs, id_prodi, alamat_mhs) VALUES ('$nim', '$nama_mhs', '$id_prodi', '$alamat_mhs')";
+                        $sql_Insert = "INSERT INTO Tbl_penjualan (nama_barang, qty, harga_satuan) VALUES ('$nama_barang', '$qty', '$harga_satuan')";
                         $result_Insert = mysqli_query($conn, $sql_Insert);
 
                         if ($result_Insert) {
                             echo "<script>
                                         alert('Data berhasil ditambahkan');
-                                        window.location.href = 'data_mhs.php';
+                                        window.location.href = 'penjualan.php';
                                     </script>";
                         } else {
                             echo "<script>
@@ -161,46 +152,36 @@ $count = mysqli_num_rows($result_tbl);
     <?php
     while ($row_edit = $result_tbl_edit->fetch_assoc()) {
     ?>
-        <div class="modal fade" id="ModalEdit<?= $row_edit["id_mhs"]; ?>">
+        <div class="modal fade" id="ModalEdit<?= $row_edit["id_penjualan"]; ?>">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
 
                     <form action="" method="POST">
                         <!-- Modal Header -->
                         <div class="modal-header">
-                            <h4 class="modal-title">Edit Data <?= $row_edit["nama_mhs"]; ?></h4>
+                            <h4 class="modal-title">Edit Data <?= $row_edit["nama_barang"]; ?></h4>
                             <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                         </div>
 
                         <!-- Modal body -->
                         <div class="modal-body">
                             <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="nama_mhs" name="nama_mhs" placeholder="Masukkan Nama Mahasiswa" value="<?= $row_edit["nama_mhs"]; ?>" required autofocus>
-                                <label for="nama_mhs" class="form-label">Nama Mahasiswa</label>
+                                <input type="text" class="form-control" id="nama_barang" name="nama_barang" placeholder="Masukkan Nama Barang" value="<?= $row_edit["nama_barang"]; ?>" required autofocus>
+                                <label for="nama_barang" class="form-label">Nama Barang</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <select class="form-select" aria-label="Floating label select example" id="id_prodi" name="id_prodi" required>
-                                    <option disabled selected>Pilih Fakultas</option>
-                                    <?php
-                                    $sql_prodi_edit = "SELECT * FROM tbl_prodi";
-                                    $result_prodi_edit = mysqli_query($conn, $sql_prodi_edit);
-                                    while ($row_prodi_edit = $result_prodi_edit->fetch_assoc()) { ?>
-                                        <option value="<?= $row_prodi_edit["id_prodi"]; ?>" <?php if ($row_prodi_edit["id_prodi"] == $row_edit["id_prodi"]) {
-                                                                                                echo "selected";
-                                                                                            } ?>> <?= $row_prodi_edit["nama_prodi"]; ?> </option>
-                                    <?php } ?>
-                                </select>
-                                <label for="id_prodi">Fakultas</label>
+                                <input type="text" class="form-control" id="qty" name="qty" placeholder="Masukkan QTY" value="<?= $row_edit["qty"]; ?>" required>
+                                <label for="qty" class="form-label">QTY</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <textarea class="form-control" placeholder="Masukkan Alamat" name="alamat_mhs" id="alamat_mhs" style="height: 100px"><?= $row_edit["alamat_mhs"]; ?></textarea>
-                                <label for="alamat_mhs">Alamat</label>
+                                <input type="text" class="form-control" id="harga_satuan" name="harga_satuan" placeholder="Masukkan Harga Satuan" value="<?= $row_edit["harga_satuan"]; ?>" required>
+                                <label for="harga_satuan" class="form-label">Harga Satuan</label>
                             </div>
                         </div>
 
                         <!-- Modal footer -->
                         <div class="modal-footer">
-                            <input type="hidden" name="id_mhs" value="<?= $row_edit["id_mhs"]; ?>">
+                            <input type="hidden" name="id_penjualan" value="<?= $row_edit["id_penjualan"]; ?>">
                             <input type="submit" class="btn btn-primary" name="edit" value="Edit">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         </div>
@@ -209,19 +190,18 @@ $count = mysqli_num_rows($result_tbl);
                     <!-- edit data ke database dengan button edit -->
                     <?php
                     if (isset($_POST["edit"])) {
-                        $id_mhs = $_POST["id_mhs"];
-                        $nama_mhs = $_POST["nama_mhs"];
-                        $id_prodi = $_POST["id_prodi"];
-                        $alamat_mhs = $_POST["alamat_mhs"];
+                        $id_penjualan = $_POST["id_penjualan"];
+                        $nama_barang = $_POST["nama_barang"];
+                        $qty = $_POST["qty"];
+                        $harga_satuan = $_POST["harga_satuan"];
 
-                        $sql_Update = "UPDATE tbl_mhs SET nama_mhs = '$nama_mhs', id_prodi = '$id_prodi', alamat_mhs = '$alamat_mhs' WHERE id_mhs = '$id_mhs'";
+                        $sql_Update = "UPDATE Tbl_penjualan SET nama_barang = '$nama_barang', qty = '$qty', harga_satuan = '$harga_satuan' WHERE id_penjualan = '$id_penjualan'";
                         $result_Update = mysqli_query($conn, $sql_Update);
 
                         if ($result_Update) {
-                            // kembali ke file data_mhs.php bukan dengan javascript
                             echo "<script>
                                         alert('Data berhasil diubah');
-                                        window.location.href = 'data_mhs.php';
+                                        window.location.href = 'penjualan.php';
                                     </script>";
                         } else {
                             echo "<script>
@@ -241,33 +221,32 @@ $count = mysqli_num_rows($result_tbl);
     <?php
     while ($row_delete = $result_tbl_delete->fetch_assoc()) {
     ?>
-        <div class="modal fade" id="ModalDelete<?= $row_delete["id_mhs"]; ?>">
+        <div class="modal fade" id="ModalDelete<?= $row_delete["id_penjualan"]; ?>">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
 
                     <form action="" method="POST">
                         <!-- Modal Header -->
                         <div class="modal-header">
-                            <h4 class="modal-title">Hapus Data <?= $row_delete["nama_mhs"]; ?></h4>
+                            <h4 class="modal-title">Hapus Data <?= $row_delete["nama_barang"]; ?></h4>
                             <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                         </div>
 
                         <!-- Modal body -->
                         <div class="modal-body">
-                            <p>Apakah anda yakin ingin menghapus data <?= $row_delete["nama_mhs"]; ?> ?</p>
+                            <p>Apakah anda yakin ingin menghapus data <?= $row_delete["nama_barang"]; ?> ?</p>
                             <div class="card mx-auto" style="width: 18rem;">
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Nama &nbsp&nbsp&nbsp : <?= $row_delete["nama_mhs"]; ?></li>
-                                    <li class="list-group-item">NIM&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp : <?= $row_delete["nrp"]; ?></li>
-                                    <li class="list-group-item">Fakultas : <?= $row_delete["nama_prodi"]; ?></li>
-                                    <li class="list-group-item">Alamat &nbsp : <?= $row_delete["alamat_mhs"]; ?></li>
+                                    <li class="list-group-item">Nama Barang &nbsp&nbsp&nbsp : <?= $row_delete["nama_barang"]; ?></li>
+                                    <li class="list-group-item">QTY&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp : <?= $row_delete["qty"]; ?></li>
+                                    <li class="list-group-item">Harga Satuan : <?= $row_delete["harga_satuan"]; ?></li>
                                 </ul>
                             </div>
                         </div>
 
                         <!-- Modal footer -->
                         <div class="modal-footer">
-                            <input type="hidden" name="id_mhs" value="<?= $row_delete["id_mhs"]; ?>">
+                            <input type="hidden" name="id_penjualan" value="<?= $row_delete["id_penjualan"]; ?>">
                             <input type="submit" class="btn btn-danger" name="delete" value="Hapus">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         </div>
@@ -276,15 +255,15 @@ $count = mysqli_num_rows($result_tbl);
                     <!-- hapus data ke database dengan button hapus -->
                     <?php
                     if (isset($_POST["delete"])) {
-                        $id_mhs = $_POST["id_mhs"];
+                        $id_penjualan = $_POST["id_penjualan"];
 
-                        $sql_Delete = "DELETE FROM tbl_mhs WHERE id_mhs = '$id_mhs'";
+                        $sql_Delete = "DELETE FROM Tbl_penjualan WHERE id_penjualan = '$id_penjualan'";
                         $result_Delete = mysqli_query($conn, $sql_Delete);
 
                         if ($result_Delete) {
                             echo "<script>
                                         alert('Data berhasil dihapus');
-                                        window.location.href = 'data_mhs.php';
+                                        window.location.href = 'penjualan.php';
                                     </script>";
                         } else {
                             echo "<script>
